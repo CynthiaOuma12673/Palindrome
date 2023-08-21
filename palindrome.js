207e744ad3dbc5dfa6ee95cb7046f6004aeb69a0
CynthiaOuma12673 <oumacynthia817@gmail.com>
2023-08-21 14:07:28 +0300

    create ispalindrome function which checks if a given string is palindrome

--- palindrome.js
+++ palindrome.js
@@ -4,3 +4,8 @@ const rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 });
+
+function isPalindrome(str){
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split("").reverse().join("");
+}
